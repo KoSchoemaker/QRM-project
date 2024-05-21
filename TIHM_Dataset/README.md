a802762329a499440e06b15677fdcd2e1f2dad52
Ko Schoemaker <koschoe@hotmail.com>
2024-05-21 12:40:21 +0200

    added readme in dataset folder

---
+++ TIHM_Dataset/README.md
@@ -0,0 +1,3 @@
+Place the dataset files in this directory. At least Activity.csv and Sleep.csv need to be present
+
+These files can be downloaded from [here](https://zenodo.org/records/7622128).
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 # imports
+import pandas as pd
+import os
+import os.path
+
+# defines
+activityPath = 'TIHM_Dataset/Activity.csv'
+sleepPath = 'TIHM_Dataset/Sleep.csv'
+
+# check if needed files exist
+if os.path.isfile(activityPath) \
+    and os.access(activityPath, os.R_OK) \
+    and os.path.isfile(sleepPath) \
+    and os.access(sleepPath, os.R_OK):
+    print('filesystem intact')
+else:
+    raise FileExistsError('check if activity.csv and sleep.csv are readable and intact')
+
+# reading csv file
+# df = pd.read_csv("people.csv")
+# print(df.head())
 
 # load data into pandas
 
