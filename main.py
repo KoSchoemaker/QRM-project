7d62587a63d0aa0cae4c9736e2c0b241445350b5
Jargogle22 <35296276+Jargogle22@users.noreply.github.com>
2024-05-22 16:59:54 +0200

    added some simple sleep schedule extraction

--- main.py
+++ main.py
@@ -26,13 +26,26 @@ demographicsDataFrame = pd.read_csv(demographicsPath)
 # get a list of all patientIds
 patientIDs = demographicsDataFrame[['patient_id']].to_numpy().flatten()
 
-print(sleepDataFrame.head())
-
-# later make a loop, for now just use one participant
-patientId = patientIDs[0]
-
 # IVs
 ## for a patient (because we're doing within-subject design):
 ### for each day:
 #### get sleep schedule (first time sleep.csv registered and last time it was registered for a certain sleep period)
 ### analyse data and create metric for consistency: get variance (?) for these two variables
+
+# later make a loop, for now just use one participant
+patientId = patientIDs[1]
+
+# a pd series with all datetimes of sleep data from the patient
+patientSleepTimes = pd.to_datetime(sleepDataFrame[sleepDataFrame['patient_id'] == patientId]['date'])
+
+timeThreshold = 3600*3 # three hours
+prevData = None
+for index, data in patientSleepTimes.items():
+    if prevData == None:
+        prevData = data
+        continue
+    
+    delta = data-prevData
+    if delta.total_seconds() > timeThreshold:
+        print(f'wake time {prevData}, next sleep time {data}')
+    prevData = data
