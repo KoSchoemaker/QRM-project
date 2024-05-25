59761a0381713a43ad8f18d6e76851c7d16f7c5d
Jargogle22 <35296276+Jargogle22@users.noreply.github.com>
2024-05-25 13:27:20 +0200

    use circular variance as measure of time consistency. added plotting file for plotting functions. made some images of plots.

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 import pandas as pd
 import os
 import os.path
-from sleepSchedule import getSleepSchedule
-import matplotlib.pyplot as plt
-import numpy as np
-from statsmodels.tsa.stattools import adfuller, kpss
+from sleepSchedule import getSleepVariance
 
 def checkFile(filePath):
     if not (os.path.isfile(filePath) and os.access(filePath, os.R_OK)):
@@ -30,21 +27,13 @@ demographicsDataFrame = pd.read_csv(demographicsPath)
 # get a list of all patientIds
 patientIds = demographicsDataFrame[['patient_id']].to_numpy().flatten()
 
-# IVs
-## for a patient (because we're doing within-subject design):
-### for each day:
-#### get sleep schedule (first time sleep.csv registered and last time it was registered for a certain sleep period)
-### analyse data and create metric for consistency: get variance (?) for these two variables
+# only consider participants that have both sleep and activity data
+patientIds = [id for id in patientIds if id in sleepDataFrame[['patient_id']].values and id in activityDataFrame[['patient_id']].values]
 
-# patientIds = [patientIds[44], patientIds[47]] # for now just use one participant. comment line for loop over all participants
+# TODO remove participants with less that x sleep data
+
+# patientIds = [patientIds[4]] # for now just use one participant. comment line for loop over all participants
 for i, patientId in enumerate(patientIds):
     print(f'-> processing patientID {i}: {patientId}')
-    (wakeTimes, sleepTimes) = getSleepSchedule(sleepDataFrame, patientId)
-
-    if len(wakeTimes) > 10 and sleepTimes != []:
-        # print(np.var(wakeTimes))
-    #     plt.plot(wakeTimes, 'ro', sleepTimes, 'gs')
-    #     plt.savefig(f'{patientId} sleep wake schedule')
-    #     plt.close()
-        print(adfuller(wakeTimes))
-        print(kpss(wakeTimes))
+    sleepVariance = getSleepVariance(sleepDataFrame, patientId)
+    print(f'sleepVariance= {sleepVariance}')
--- sleepSchedule.py
+++ sleepSchedule.py
@@ -1,15 +1,27 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotting
+from cmath import rect, phase
+from math import radians, degrees
 
 def getSleepSchedule(sleepDataFrame, patientId):
-    # a pd series with all datetimes of sleep data from the patient
+    # a pd series with all DateTime obj of sleep data from the patient
     patientSleepTimes = pd.to_datetime(sleepDataFrame[sleepDataFrame['patient_id'] == patientId]['date'])
 
-    # no sleep data
+    # no sleep data. should already have been filtered out in main.py
     if len(patientSleepTimes) == 0:
         print('-> no sleep data for patient')
         return ([],[])
     
-    timeThreshold = 3600*3 # three hours
+    # the minimum time between one and another sleep event. If two subsequent events are further apart than this value,
+    #   count as separate events, so record a wake and sleep time for this.
+    minimumInterval = 3600*3 # three hours
+
+    # the minimum duration that sleep has to occur for us to record it as such. This mainly exists to filter out
+    #   naps during the daytime
+    minimumDuration = 3600*2 # two hours TODO
+
     prevData = None
     wakeTimes = []
     sleepTimes = []
@@ -19,14 +31,53 @@ def getSleepSchedule(sleepDataFrame, patientId):
             continue
         
         delta = data-prevData
-        if delta.total_seconds() > timeThreshold:
-            wakeTimes.append(prevData.hour + (prevData.minute / 60))
-            sleepTimes.append(data.hour + (data.minute / 60))
-            # print(f'wake time {prevData}, next sleep time {data}')
+        if delta.total_seconds() > minimumInterval:
+            wakeTimes.append((prevData.hour, prevData.minute))
+            sleepTimes.append((data.hour, data.minute))
         prevData = data
 
     return (wakeTimes, sleepTimes)
 
 # TODO normalize days (only look at times, perhaps using just a number of seconds/minutes into the day)
-# TODO exclude naps?
+# TODO exclude naps
 # TODO compare values (measure variance?)
+
+def getTimesAngles(times):
+    return np.array([((h * 60 + m) / (24 * 60)) * 2 * np.pi for h,m in times])
+
+def getCircularVariance(times):
+    angles = getTimesAngles(times)
+
+    # Calculate the mean resultant length
+    C = np.mean(np.cos(angles))
+    S = np.mean(np.sin(angles))
+    R = np.sqrt(C**2 + S**2)
+
+    # Calculate the circular variance
+    circular_variance = 1 - R
+    return circular_variance
+
+def getSleepVariance(sleepDataFrame, patientId, plotCircle = False):
+    wakeSchedule, sleepSchedule = getSleepSchedule(sleepDataFrame, patientId)
+    wakeCircularVariance = getCircularVariance(wakeSchedule)
+    sleepCircularVariance = getCircularVariance(sleepSchedule)
+    if plotCircle:
+        plotting.wakeSleepCircle(wakeSchedule, sleepSchedule, patientId)
+    return (wakeCircularVariance, sleepCircularVariance)
+
+# depricated
+def mean_angle(deg):
+    return degrees(phase(sum(rect(1, radians(d)) for d in deg)/len(deg)))
+
+# depricated
+def mean_time(times):
+    seconds = ((float(s) + int(m) * 60 + int(h) * 3600) for h, m, s in times)
+    day = 24 * 60 * 60
+    to_angles = [s * 360. / day for s in seconds]
+    mean_as_angle = mean_angle(to_angles)
+    mean_seconds = mean_as_angle * day / 360.
+    if mean_seconds < 0:
+        mean_seconds += day
+    h, m = divmod(mean_seconds, 3600)
+    m, s = divmod(m, 60)
+    return '%02i:%02i:%02i' % (h, m, s)
