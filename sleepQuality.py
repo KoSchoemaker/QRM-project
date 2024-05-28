7773287d733adcfb5c3e469a06206b03d70f6136
Ko Schoemaker <koschoe@hotmail.com>
2024-05-28 12:24:33 +0200

    sleepquality dv framework, added clustered circular plots

---
+++ sleepQuality.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def getQualityMetrics(sleepDataframe, patientId):
+    # loop over patients, give patient sleep data, create dataframe with info for all patients
+    return []
+
+def getPatientMetrics(patientSleep):
+    # get values for one patients
+    return []
+
+def getSleepEfficiency(patientSleep):
+    # Total sleep time to time in bed ratio
+    pass
+
+def getSleepLatency(patientSleep):
+    # Time in minutes to transition from awake to sleep
+    pass
+
+def getREMDistribution(patientSleep):
+    # Ratio of REM time to total sleep time
+    pass
+
+def getLightSleepDistribution(patientSleep):
+    # Total sleep time to time in bed ratio
+    pass
+
+def getDeepSleepDistribution(patientSleep):
+    # Ratio of Deep sleep time to total sleep time
+    pass
+
+def getAwakenings(patientSleep):
+    # Number of times in which individuals became awake, >5 min
+    pass
+
+def getWakeAfterSleepOnset(patientSleep):
+    # Time spent awake after sleep has started and before final wake-up
+    pass
