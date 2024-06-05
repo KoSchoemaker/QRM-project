19c61f0e91af7f338d4f9132e96629b3ed1f097c
Ko Schoemaker <koschoe@hotmail.com>
2024-06-05 12:31:43 +0200

    cleanup + intermediate result writing

--- clustering.py
+++ clustering.py
@@ -11,22 +11,22 @@ y = np.array([y[1] for y in list(data.values())])
 
 xy = np.concatenate((x.reshape(-1,1), y.reshape(-1,1)), axis=1)
 
-values = np.array(list(data.values()))
+keys = []
+values = []
+for key, value in data.items():
+    values.append(value)
+    keys.append(key)
+
+# values = np.array(list(data.values()))
 kmeans = KMeans(n_clusters=2)
 kmeans.fit(xy)
-# x = values
-# y = [0 for i in values]
-# plt.plot([0,0.5], [0,0.5])
+
 plt.scatter(x, y, c=kmeans.labels_)
 plt.show()
 plt.close()
 
-# X = xy
-# mean_shift = MeanShift()
-# mean_shift.fit(X)
+patientClusteringDict = {key: int(kmeans.labels_[i]) for i, key in enumerate(keys)}
 
-# plt.scatter(X[:, 0], X[:, 1], c=mean_shift.labels_, cmap='viridis', marker='o')
-# # plt.scatter(mean_shift.cluster_centers_[:, 0], mean_shift.cluster_centers_[:, 1], c='red', marker='x', s=200, linewidths=3, label='Cluster Centers')
-# plt.title('Mean Shift Clustering')
-# plt.legend()
-# plt.show()
+import json
+with open('intermediate_results/patient_clustering_results.json', 'w') as f:
+    json.dump(patientClusteringDict, f)
